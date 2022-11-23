b3951569e38ee0d1a73adeabb6607f295d2614af
Corey Daniels <c.daniels@alkime.co>
2022-11-23 08:57:04 -0500

    Make Vigenere a struct

--- session-one/src/vigenere.rs
+++ session-one/src/vigenere.rs
@@ -1,53 +1,73 @@
-pub fn encode(text: &str, key: &str) -> String {
-    let text = text
-        .to_uppercase()
-        .chars()
-        .filter(|c| c.is_alphabetic())
-        .collect::<String>();
-
-    text.chars()
-        .zip(key.chars().cycle())
-        .map(|(text_char, key_char)| {
-            let key_index = key_char as u8 - b'A';
-            let text_index = text_char as u8 - b'A';
-            let new_index = (text_index + key_index) % 26;
-
-            (new_index + b'A') as char
-        })
-        .collect()
+pub struct Vigenere {
+    key: String,
 }
 
-pub fn decode(cipher: &str, key: &str) -> String {
-    cipher
-        .chars()
-        .zip(key.chars().cycle())
-        .map(|(cipher_char, key_char)| {
-            let key_index = key_char as u8 - b'A';
-            let cipher_index = cipher_char as u8 - b'A';
-            let new_index = (cipher_index + 26 - key_index) % 26;
+impl Vigenere {
+    pub fn new(key: &str) -> Self {
+        Self {
+            key: key
+                .to_uppercase()
+                .chars()
+                .filter(|c| c.is_alphabetic())
+                .collect::<String>(),
+        }
+    }
 
-            (new_index + b'A') as char
-        })
-        .collect()
+    fn parse_input(text: &str) -> String {
+        text.to_uppercase()
+            .chars()
+            .filter(|c| c.is_alphabetic())
+            .collect()
+    }
+
+    pub fn encode(&self, text: &str) -> String {
+        Self::parse_input(text)
+            .chars()
+            .zip(self.key.chars().cycle())
+            .map(|(text_char, key_char)| {
+                let key_index = key_char as u8 - b'A';
+                let text_index = text_char as u8 - b'A';
+                let new_index = (text_index + key_index) % 26;
+
+                (new_index + b'A') as char
+            })
+            .collect()
+    }
+
+    pub fn decode(&self, cipher: &str) -> String {
+        Self::parse_input(cipher)
+            .chars()
+            .zip(self.key.chars().cycle())
+            .map(|(cipher_char, key_char)| {
+                let key_index = key_char as u8 - b'A';
+                let cipher_index = cipher_char as u8 - b'A';
+                let new_index = (cipher_index + 26 - key_index) % 26;
+
+                (new_index + b'A') as char
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::vigenere::{decode, encode};
+    use crate::vigenere::Vigenere;
 
     #[test]
     fn encode_works() {
+        let vigenere = Vigenere::new("lemon");
         let text = "attackatdawn";
-        let key = "LEMON";
-        let encrypted = encode(text, key);
+
+        let encrypted = vigenere.encode(text);
         assert_eq!(encrypted, "LXFOPVEFRNHR");
     }
 
     #[test]
     fn decode_works() {
-        let text = "LXFOPVEFRNHR";
-        let key = "LEMON";
-        let decrypted = decode(text, key);
+        let vigenere = Vigenere::new("lemon");
+        let text = "lxfopvefrnhr";
+
+        let decrypted = vigenere.decode(text);
         assert_eq!(decrypted, "ATTACKATDAWN");
     }
 }
